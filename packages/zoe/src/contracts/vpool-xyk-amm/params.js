7199aceb6a065cfdd327c171389b2f4b5d6171fe
Chris Hibbert <hibbert@agoric.com>
2021-12-07 11:19:17 -0800

    feat!: add governed electorate to contractGovernor
    This commit contains only the changes in the governance package. Changes to
    Treasury and AMM are in separate commits, but tests won't pass without all
    layers
    
    Params in terms switch from arrays with embedded names to named records.
    The electorate is passed to contracts in an invitation rather than a facet.
    
    Dropped some tests in test-paramGovernance as the use of fakes was unsustainable.
       The two that were dropped were a happy path, and an inaccessible error path.

--- packages/zoe/src/contracts/vpool-xyk-amm/params.js
+++ packages/zoe/src/contracts/vpool-xyk-amm/params.js
@@ -1,14 +1,41 @@
 // @ts-check
 
 import { makeParamManagerBuilder } from '@agoric/governance/src/paramGovernance/paramManager.js';
+import { CONTRACT_ELECTORATE } from '@agoric/governance/src/paramGovernance/governParam.js';
+import {
+  makeGovernedNat,
+  makeGovernedInvitation,
+} from '@agoric/governance/src/paramGovernance/paramMakers.js';
 
 export const POOL_FEE_KEY = 'PoolFee';
 export const PROTOCOL_FEE_KEY = 'ProtocolFee';
 
+const POOL_FEE_BP = 24n;
+const PROTOCOL_FEE_BP = 6n;
+
 /** @type {(poolFeeBP: bigint, protocolFeeBP: bigint) => ParamManagerFull} */
-export const makeParamManager = (poolFeeBP, protocolFeeBP) => {
-  return makeParamManagerBuilder()
+const makeParamManager = async (poolFeeBP, protocolFeeBP) => {
+  const builder = makeParamManagerBuilder()
     .addNat(POOL_FEE_KEY, poolFeeBP)
-    .addNat(PROTOCOL_FEE_KEY, protocolFeeBP)
-    .build();
+    .addNat(PROTOCOL_FEE_KEY, protocolFeeBP);
+
+  await builder.addInvitation(CONTRACT_ELECTORATE, poserInvitation);
+  return builder.build();
 };
+
+const makeAmmParams = (
+  electorateInvitationAmount,
+  protocolFeeBP,
+  poolFeeBP,
+) => {
+  return harden({
+    [POOL_FEE_KEY]: makeGovernedNat(poolFeeBP),
+    [PROTOCOL_FEE_KEY]: makeGovernedNat(protocolFeeBP),
+    [CONTRACT_ELECTORATE]: makeGovernedInvitation(electorateInvitationAmount),
+  });
+};
+
+harden(makeAmmParams);
+harden(makeParamManager);
+
+export { makeAmmParams, makeParamManager };
